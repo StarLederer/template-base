b947cb71d49c8559e764f6d5753b76a96260a3a5
Herman Lederer <germans.lederers@gmail.com>
2022-11-20 14:38:57 +0100

    Convert nix shell to flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell rec {
+          nativeBuildInputs = with pkgs; [];
+
+          buildInputs = with pkgs; [];
+        };
+      }
+    );
+}
